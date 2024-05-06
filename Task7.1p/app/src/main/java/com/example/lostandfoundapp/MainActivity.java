12c14652ea2a8c67fc9044954be2dce2fb5cb96f
Roshan <roshanjoseapr6@gmail.com>
2024-05-06 15:18:52 +1000

    mainactivity
    changed

--- Task7.1p/app/src/main/java/com/example/lostandfoundapp/MainActivity.java
+++ Task7.1p/app/src/main/java/com/example/lostandfoundapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonNewAdd = findViewById(R.id.create_new_ad);
+        Button buttonShowAll = findViewById(R.id.show_all_ads);
+        buttonNewAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewAdd.class);
+                startActivity(intent);
+            }
+        });
 
-    }
-    public void launchNewAddPage(View view) {
-        Intent intent = new Intent(MainActivity.this, NewAdd.class);
-        startActivity(intent);
-    }
-
-    public void launchShowAllPage(View view) {
-        Intent intent = new Intent(MainActivity.this, ShowAllAddspage.class);
-        startActivity(intent);
+        buttonShowAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShowAllAddspage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
